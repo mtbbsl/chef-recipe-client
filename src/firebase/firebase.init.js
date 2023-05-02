70254eb13f564b179ab3d6becb8d047ddae1d7cf
Mahtab Mustakim <mtbbsl@gmail.com>
2023-05-02 15:37:13 +0600

    first commit chef recipe client

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD7haBzMCFGfQAKHWbudCPj6GMDUeXqbgI",
+  authDomain: "chef-recipe-f3b44.firebaseapp.com",
+  projectId: "chef-recipe-f3b44",
+  storageBucket: "chef-recipe-f3b44.appspot.com",
+  messagingSenderId: "52878803958",
+  appId: "1:52878803958:web:d896754ac92ae031d4bdd7",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
