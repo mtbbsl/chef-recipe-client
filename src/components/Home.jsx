6aa6c67f6851e33fbcb84d6168b232ea624dc177
Mahtab Mustakim <mtbbsl@gmail.com>
2023-05-04 05:31:28 +0600

    added chefs cards

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,48 +1,60 @@
-import React, { useContext } from 'react';
-import { AuthContext } from '../providers/AuthProviders';
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../providers/AuthProviders";
 
 const Home = () => {
-    const {user} = useContext(AuthContext);
-    // console.log(user);
-    return (
-      <div>
-        <section>
-          <div className="hero min-h-screen bg-base-200">
-            <div className="hero-content flex-col lg:flex-row">
-              <img src="/chef.png" className="max-w-sm rounded-lg shadow-2xl" />
-              <div>
-                <h1 className="text-5xl font-bold">Indian Chef</h1>
-                <p className="py-6">
-                  Six Famous Indian Chefs That Will Help You Unlock Your
-                  Culinary Skills The Right Way
-                </p>
-              </div>
-            </div>
-          </div>
-        </section>
+  const { user } = useContext(AuthContext);
+  // console.log(user);
 
-        <section>
-          <div className="card card-side bg-base-100 shadow-xl">
-            <figure>
-              <img
-                src="/chef.png"
-                alt=""
-              />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">Chef Name: Ranvir Kumer</h2>
-              <p>Year of Experience: 7</p>
-              <p>Numbers of recipes: 10</p>
-              <p>Likes: 5010</p>
-              <div className="card-actions justify-end">
-                <button className="btn btn-primary">View Recipes</button>
-              </div>
+  const [chefsData, setChefsData] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/chefs")
+      .then((res) => res.json())
+      .then((data) => setChefsData(data))
+      .catch((error) => console.error(error));
+  }, []);
+
+  return (
+    <div>
+      {/* banner section */}
+      <section>
+        <div className="hero min-h-screen bg-base-200">
+          <div className="hero-content flex-col lg:flex-row">
+            <img src="/chef.png" className="max-w-sm rounded-lg shadow-2xl" />
+            <div>
+              <h1 className="text-5xl font-bold">Indian Chef</h1>
+              <p className="py-6">
+                Six Famous Indian Chefs That Will Help You Unlock Your Culinary
+                Skills The Right Way
+              </p>
             </div>
           </div>
-          
-        </section>
-      </div>
-    );
+        </div>
+      </section>
+
+      {/* chefs card section */}
+      <section className="px-10 py-10">
+        <div className="grid gap-10 lg:grid-cols-3 sm:grid-cols-1">
+          {chefsData.map((chef) => (
+            <div key={chef.id} className="card card-side bg-neutral shadow-xl">
+              <figure>
+                <img src={chef.chef_picture_url} alt="" className="" />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">Chef Name: {chef.chef_name}</h2>
+                <p>Experience: {chef.years_of_experience} years</p>
+                <p>No. of Recipes: {chef.number_of_recipes}</p>
+                <p>Likes: {chef.likes}</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-active btn-accent">View Recipes</button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Home;
